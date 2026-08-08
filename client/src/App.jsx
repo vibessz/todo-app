e8960d0b37bbddf135a0125bfa4694f73193d1af
vibessz <192662446+vibessz@users.noreply.github.com>
2026-08-07 21:00:34 -0500

    Added edit tasks feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   const [tasks, setTasks] = useState([])
   const [newTask, setNewTask] = useState('')
   const [editingId, setEditingId] = useState(null)
+  const [editingTask, setEditingTask] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:3000/tasks')
@@ -16,8 +17,32 @@ function App() {
       <h1>My Tasks</h1>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>{task.text} 
-          
+          <li key={task.id} onClick={() => {setEditingId(task.id); setEditingTask(task.text); console.log(task.id);}}>
+            
+            
+            {task.id === editingId ? 
+            (
+                
+
+              <form onSubmit={
+                                (e) => {e.preventDefault()
+                                fetch(`http://localhost:3000/tasks/${task.id}`, {
+                                  method: 'PUT',
+                                  headers: { 'Content-Type': 'application/json' },
+                                  body: JSON.stringify({text: editingTask})
+                                })
+                                .then((res) => res.json())
+                                .then((data) => setTasks(data))
+                                .then(() => setEditingTask("")).then(() => setEditingId(null))
+                                }
+                              }>
+            
+            <input autoFocus type='text' value={editingTask} onChange={(e) => setEditingTask(e.target.value)}></input>
+            </form>
+            ) 
+              
+              : (task.text)}
+
           <button onClick={(e) => (
 
             fetch(`http://localhost:3000/tasks/${task.id}`, {
@@ -26,9 +51,7 @@ function App() {
 
 
           )}>Del</button>
-          
-          
-          
+
           </li>         
         ))}
       </ul>
